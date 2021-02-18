a34f9da46551ddb8076adc70f78245bb776d3fb5
JaideepArwani <77190655+JaideepArwani@users.noreply.github.com>
2021-02-18 17:10:50 +0800

    Add files via upload

---
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication11;
+
+/**
+ *
+ * @author admin
+ */
+public class JavaApplication11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+        
+            String[] arr= new String[6];
+    
+    arr[0]="101";
+    arr[1]="deepsk";
+    arr[2]="deepsk@gmsil.com";
+    
+    
+    arr[3]="102";
+    arr[4]="ali";
+    arr[5]="ali@gmail.com";
+    
+    for (int t = 0; t < 6; t++)
+    {
+        System.out.println(arr[t]);
+    }
+       
+    }
+    
+}
